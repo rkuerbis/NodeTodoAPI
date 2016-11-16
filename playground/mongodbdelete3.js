bc72af3d6d714141419c6fbaa8c141cb3597bfbf
Ralph Kuerbis <ralph@kuerbis.ca>
2016-11-16 02:26:13 -0800

    Third Commit

---
+++ playground/mongodbdelete3.js
@@ -0,0 +1,43 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // same as above
+
+// ES6 destructuring
+// var user = {name: 'Andrew', age: 25};
+// var {name} = user;
+// console.log(name);
+// //
+// var obj = new ObjectID();
+// console.log(obj);
+// console.log(JSON.stringify(obj.getTimestamp(), undefined, 2));
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+
+  // Insert new doc into Users (name, age, location)
+
+  db.collection('Users').deleteMany({ name: 'Ralph Kuerbis' }).then((result) => {
+    console.log(result);
+//    console.log(JSON.stringify(docs, undefined, 2));
+
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.collection('Users').find({ name: "Ralph Kuerbis" }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+
+//    console.log(JSON.stringify(docs, undefined, 2));
+
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+
+  //db.close();
+});
---
+++ playground/mongodbdelete4.js
@@ -0,0 +1,45 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // same as above
+
+// ES6 destructuring
+// var user = {name: 'Andrew', age: 25};
+// var {name} = user;
+// console.log(name);
+// //
+// var obj = new ObjectID();
+// console.log(obj);
+// console.log(JSON.stringify(obj.getTimestamp(), undefined, 2));
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+
+  // Insert new doc into Users (name, age, location)
+
+  db.collection('Users').deleteOne({
+    _id: new ObjectID("582c32bf8c175eddee1902a8")
+  }).then((result) => {
+    console.log(result);
+//    console.log(JSON.stringify(docs, undefined, 2));
+
+  }, (err) => {
+    console.log('Unable to fetch todos', err);
+  });
+
+  db.collection('Users').find({ name: "Ralph Kuerbis" }).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+
+//    console.log(JSON.stringify(docs, undefined, 2));
+
+  }, (err) => {
+    console.log('Unable to fetch users', err);
+  });
+
+
+  //db.close();
+});
